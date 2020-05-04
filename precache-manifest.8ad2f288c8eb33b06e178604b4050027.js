04a39f9e0f0b7eecf31981275507e72a9c1e53d1
unknown <risnarendra@gmail.com>
2020-05-04 14:44:26 +0530

    Updates

---
+++ precache-manifest.8ad2f288c8eb33b06e178604b4050027.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e925d73ef6a19f811c352294d70bbf",
+    "url": "/LatestNBA/index.html"
+  },
+  {
+    "revision": "988657eff895a5da1b04",
+    "url": "/LatestNBA/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "1366efd47d9ed7b12d22",
+    "url": "/LatestNBA/static/css/main.f2e0fb94.chunk.css"
+  },
+  {
+    "revision": "988657eff895a5da1b04",
+    "url": "/LatestNBA/static/js/2.ecfc97fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/LatestNBA/static/js/2.ecfc97fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1366efd47d9ed7b12d22",
+    "url": "/LatestNBA/static/js/main.5a1c0d64.chunk.js"
+  },
+  {
+    "revision": "4c35eb886cdccee650e6",
+    "url": "/LatestNBA/static/js/runtime-main.bea1838c.js"
+  }
+]);
